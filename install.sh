7ae11567543ced8dd77755fd23dcf36f9e260c5c
Sam Bernheim <sambernheim@gmail.com>
2016-07-21 11:47:27 -0400

    have a workaround

--- install.sh
+++ install.sh
@@ -1,27 +1,31 @@
 #!/bin/bash
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+CYAN='\033[0;36m'
+NC='\033[0;00m' # No Color
+
 install_brew() {
 	which -s brew
-	if [ $? != 0 ] ; then
+	if [[ $? != 0 ]] ; then
 		# Install Homebrew
-		echo -e "${CYAN}******Installing Homebrew ${NC}"
-		/usr/bin/ruby -e "$(curl -fsSL https://raw.github.com/gist/323731)"
+		# # echo -e "${CYAN}InstallingHomebrew${NC}"
+		/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 	else
 		# Update homebrew
-		echo -e "${CYAN}******Updating Homebrew ${NC}"
-		brew update
+		# # echo -e "${CYAN}UpdatingHomebrew${NC}"
+		brew update > output.txt
 	fi
 }
 
 download_dotfiles() {
 	# Actual dotfiles
-	echo -e "${CYAN}******Downloading dotfiles for vim, tmux ${NC}"
-	cd ~
-	git clone https://github.com/sbernheim4/dotfiles.git
-	cd ~
+	# echo -e "${CYAN}Downloading dotfiles for vim, tmux${NC}"
+	git clone https://github.com/sbernheim4/dotfiles.git > output.txt
 }
 
 install_tmux() {
-	brew install tmux
+	brew install tmux > output.txt
 
 	if [ ! -e "~/.tmux.conf" ] ; then
 		touch .tmux.conf
@@ -32,7 +36,7 @@ install_tmux() {
 
 install_vim() {
 
-	brew install vim
+	brew install vim > output.txt
 
 	if [ ! -e "~/.vimrc" ] ; then
 		touch .vimrc
@@ -40,7 +44,7 @@ install_vim() {
 
 	ln -sf ~/dotfiles/.vimrc ~/.vimrc
 
-	echo "${CYAN}******Creating ~/.vim, ~/.vim/autoload and ~/.vim/colors and using Vim Plug for plugin manager for vim"
+	# echo "${CYAN}Creating ~/.vim, ~/.vim/autoload and ~/.vim/colors and using Vim Plug for plugin manager for vim${NC}"
 
 	if [! -d "~/.vim" ] ; then
 		mkdir ~/.vim/
@@ -56,15 +60,13 @@ install_vim() {
 		mkdir autoload
 	fi
 
-	cd ~
-
-	cp ~/dotfiles/plug.vim ~/.vim/autoload/
-	cp ~/dotfiles/onedark.vim ~/.vim/colors/
+	cp ~/dotfiles/plug.vim ~/.vim/autoload/ > output.txt
+	cp ~/dotfiles/onedark.vim ~/.vim/colors/ > output.txt
 }
 
 install_zsh() {
 
-	brew install zsh
+	brew install zsh > output.txt
 
 	if [ ! -e "~/.zshrc" ] ; then
 		touch .zshrc
@@ -73,62 +75,52 @@ install_zsh() {
 	ln -sf ~/dotfiles/.zshrc ~/.zshrc
 
 	# Install  oh-my-zsh
-	echo -e "${CYAN}******Installing oh-my-zsh ${NC}"
-	sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+	# echo -e "${CYAN}Installing oh-my-zsh${NC}"
+	sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" > output.txt
 
 	# Move theme
-	echo -e "${CYAN}******Moving honukai theme to ~/.oh-my-zsh/themes ${NC}"
+	# echo -e "${CYAN}Moving honukai theme to ~/.oh-my-zsh/themes${NC}"
 	cp ~/dotfiles/honukai.zsh-theme ~/.oh-my-zsh/themes
 
 	# Change default shell to be zsh
-	echo -e "${CYAN}******Changing default shell to ZSH ${NC}"
-	chsh -s $(which zsh)
+	# echo -e "${CYAN}Changing default shell to ZSH${NC}"
+	chsh -s $(which zsh) > output.txt
 }
 
 install_vim_packages() {
-	echo -e "${CYAN}******Installing Vim Packages. ONCE VIM HAS FINISHED INSTALLING TYPE :q <ENTER> AND REPEAT UNTIL YOU RETURN TO THE SHELL.${NC}"
+	# echo -e "${CYAN}Installing Vim Packages. O${NC}E VIM HAS FINISHED INSTALLING TYPE :q <ENTER> AND REPEAT UNTIL YOU RETURN TO THE SHELL.${NC}"
 	vim +PlugInstall
 
 	cd ~/.vim/plugged/YouCompleteMe/
-	./install.sh
+	./install.sh > output.txt
 }
 
-print_done() {
-	echo
-	echo
-	echo
 
-	echo -e "${GREEN}
-		 _________                                     __           ._.
-		\_   ___ \  ____   ____    ________________ _/  |_  ______ | |
-		/    \  \/ /  _ \ /    \  / ___\_  __ \__  \\   __\/  ___/ | |
-		\     \___(  <_> )   |  \/ /_/  >  | \// __ \|  |  \___ \   \|
-		 \______  /\____/|___|  /\___  /|__|  (____  /__| /____  >  __
-    	     \/            \//_____/            \/          \/   \/
-		_____.___.           /\                        .__  .__                   __    ._. ._.
-		\__  |   | ____  __ _)/______   ____    _____  |  | |  |     ______ _____/  |_  | | | |
-		/   |   |/  _ \|  |  \_  __ \_/ __ \   \__  \ |  | |  |    /  ___// __ \   __\  | | | |
-		\____   (  <_> )  |  /|  | \/\  ___/    / __ \|  |_|  |__  \___ \\  ___/|  |     \|  \|
-		/ ______|\____/|____/ |__|    \___  >  (____  /____/____/ /____  >\___  >__|    __  __
-		\/                                \/        \/                 \/     \/        \/  \/
-
-	"
-	echo
-	echo
-	echo -e "${NC}******"
-}
-
-# MAIN
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-CYAN='\033[0;36m'
-NC='\033[0;00m' # No Color
-
-echo -e "${CYAN}Beginning installation${NC}"
+#Main
+# echo -e "${CYAN}Beginning installation${NC}"
 
 $(install_brew)
+cd ~
 $(download_dotfiles)
+cd ~
 $(install_vim)
+cd ~
 $(install_zsh)
+cd ~
 $(install_tmux)
-$(print_done)
+cd ~
+
+echo -e "${GREEN}
+	 _________                                     __           ._.
+	\_   ___ \  ____   ____    ________________ _/  |_  ______ | |
+	/    \  \/ /  _ \ /    \  / ___\_  __ \__  \\   __\/  ___/ | |
+	\     \___(  <_> )   |  \/ /_/  >  | \// __ \|  |  \___ \   \|
+	 \______  /\____/|___|  /\___  /|__|  (____  /__| /____  >  __
+		 \/            \//_____/            \/          \/   \/
+	_____.___.           /\                        .__  .__                   __    ._. ._.
+	\__  |   | ____  __ _)/______   ____    _____  |  | |  |     ______ _____/  |_  | | | |
+	/   |   |/  _ \|  |  \_  __ \_/ __ \   \__  \ |  | |  |    /  ___// __ \   __\  | | | |
+	\____   (  <_> )  |  /|  | \/\  ___/    / __ \|  |_|  |__  \___ \\  ___/|  |     \|  \|
+	/ ______|\____/|____/ |__|    \___  >  (____  /____/____/ /____  >\___  >__|    __  __
+	\/                                \/        \/                 \/     \/        \/  \/
+"
