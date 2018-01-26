d69dc3ed51718d8c3242cb5552ba7e8e30fc91ab
sbernheim <sambernheim@gmail.com>
2018-01-25 22:16:31 -0500

    Cut off music length in tmux status line if greater than 35 characters

--- music.sh
+++ music.sh
@@ -2,18 +2,25 @@
 # Returns the current playing song in Spotify for OSX
 
 tell application "Spotify"
-  if it is running then
-    if player state is playing then
-      set track_name to name of current track
-      set artist_name to artist of current track
+if it is running then
+	if player state is playing then
+		set track_name to name of current track
+		set artist_name to artist of current track
 
-      if artist_name > 0
-        # If the track has an artist set and is therefore most likely a song rather than an advert
-        "♫ " & artist_name & " - " & track_name
-      else
-        # If the track doesn't have an artist set and is therefore most likely an advert rather than a song
-        "~ " & track_name
-      end if
-    end if
-  end if
+		if artist_name > 0
+			# If the track has an artist set and is therefore most likely a song rather than an advert
+			set t to "♫ " & artist_name & " - " & track_name
+
+			if length of t > 31
+				text 1 thru 31 of t & "..."
+			else
+				"♫ " & artist_name & " - " & track_name
+			end if
+
+		else
+			# If the track doesn't have an artist set and is therefore most likely an advert rather than a song
+			"~ " & track_name
+		end if
+	end if
+end if
 end tell
