f3f1786257ec98c3ce914cdeefb01fb904d7bd5b
sbernheim <sambernheim@gmail.com>
2018-01-27 13:54:24 -0500

    Fix battery.sh

--- battery.sh
+++ battery.sh
@@ -5,7 +5,7 @@ doubleDigit=$(pmset -g batt | grep [0-9][0-9]% | awk 'NR==1{print $3}' | cut -c 
 if [ ${#doubleDigit} -gt 1 ]; then
 	echo "$doubleDigit"
 else
-	doubleDigit=pmset -g batt | grep [0-9]% | awk 'NR==1{print $3}' | cut -c 1-2
+	doubleDigit=$(pmset -g batt | grep [0-9]% | awk 'NR==1{print $3}' | cut -c 1-2)
 	echo "$doubleDigit"
 fi
 
