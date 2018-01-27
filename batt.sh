350dc3aaff78a42cece7c9712dbf42ab1c6660e9
sbernheim <sambernheim@gmail.com>
2018-01-27 13:51:24 -0500

    Fix battery.sh to handle single digit percentage

--- batt.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-pmset -g batt | grep [0-9][0-9]% | awk 'NR==1{print $3}' | cut -c 1-3
---
+++ battery.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+doubleDigit=$(pmset -g batt | grep [0-9][0-9]% | awk 'NR==1{print $3}' | cut -c 1-3)
+
+if [ ${#doubleDigit} -gt 1 ]; then
+	echo "$doubleDigit"
+else
+	doubleDigit=pmset -g batt | grep [0-9]% | awk 'NR==1{print $3}' | cut -c 1-2
+	echo "$doubleDigit"
+fi
+
