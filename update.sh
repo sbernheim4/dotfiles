9af236b2a74be8276f8faacf566be34ee4574898
sbernheim4 <sambernheim@gmail.com>
2019-06-11 14:14:25 -0400

    Update update.sh script

--- update.sh
+++ update.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
 
-cd ~/dotfiles && git pull
-
 brew update
 
 brew upgrade
 
 npm update -g
+
+# Update dotfiles
+cd ~/dotfiles && git pull && cd
+
+# Update powerlevel9k
+cd ~/.oh-my-zsh/custom/themes/powerlevel9k && git pull && cd
+
+# Update community gruvbox
+cd ~/personal/gruvbox/ && git pull && cd
+
