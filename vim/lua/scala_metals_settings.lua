e627fbbd700ec8e3e3c445ef3f2f169baee2838e
sbernheim4 <sambernheim@gmail.com>
2021-10-02 19:18:27 -0400

    fix: metals config

--- vim/lua/scala_metals_settings.lua
+++ vim/lua/scala_metals_settings.lua
@@ -1,30 +1,22 @@
 local cmd = vim.cmd
+local g = vim.g
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 
-----------------------------------
--- COMMANDS ----------------------
-----------------------------------
--- LSP
+metals_config = require("metals").bare_config
+
 cmd([[augroup lsp]])
 cmd([[autocmd!]])
 cmd([[autocmd FileType scala setlocal omnifunc=v:lua.vim.lsp.omnifunc]])
 cmd([[autocmd FileType scala,sbt lua require("metals").initialize_or_attach(metals_config)]])
 cmd([[augroup end]])
 
--- Need for symbol highlights to work correctly
-cmd([[hi! link LspReferenceText CursorColumn]])
-cmd([[hi! link LspReferenceRead CursorColumn]])
-cmd([[hi! link LspReferenceWrite CursorColumn]])
+vim.cmd([[hi! link LspReferenceText CursorColumn]])
+vim.cmd([[hi! link LspReferenceRead CursorColumn]])
+vim.cmd([[hi! link LspReferenceWrite CursorColumn]])
 
-----------------------------------
--- LSP Setup ---------------------
-----------------------------------
-metals_config = require("metals").bare_config
 
--- Example of settings
 metals_config.settings = {
-  showImplicitArguments = true,
-  excludedPackages = { "akka.actor.typed.javadsl", "com.github.swagger.akka.javadsl" },
+    showImplicitArguments = true,
 }
 
 capabilities.textDocument.completion.completionItem.snippetSupport = true
