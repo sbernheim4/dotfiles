3afa07dbbfa333f1f676ae8a4af0b3113e1667d7
Sam Bernheim <sbernheim4@users.noreply.github.com>
2022-05-23 23:53:09 -0400

    fix: zshrc soft link spelling

--- scripts/install.sh
+++ scripts/install.sh
@@ -177,7 +177,7 @@ function useZsh() {
     cd ~/
 
     # Link .zshrc
-    ln -sf ~/dotfiles/zsh/.zshrc ~/.zhsrc
+    ln -sf ~/dotfiles/zsh/.zshrc ~/.zshrc
 
     # Use powerlevel10k for prompt - https://github.com/romkatv/powerlevel10k
     git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
