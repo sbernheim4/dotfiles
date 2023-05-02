c6615c8240b8263a716519a9053f9919bc2d3c71
sbernheim4 <sambernheim@gmail.com>
2023-05-02 10:38:30 -0400

    chore: fix indentation

--- vim/lua/lsp_signature_settings.lua
+++ vim/lua/lsp_signature_settings.lua
@@ -1,11 +1,12 @@
 local lsp_signature = require('lsp_signature')
 
 lsp_signature.setup({
-    bind = true, -- This is mandatory, otherwise border config won't get registered.
-    handler_opts = {
-        border = 'single'
-    },
-    padding=' ',
-    hint_prefix = "",
-    hint_enable = false
+  bind = true,   -- This is mandatory, otherwise border config won't get registered.
+  handler_opts = {
+    border = 'single'
+  },
+  padding = ' ',
+  hint_prefix = "",
+  hint_enable = false,
+  floating_window_above_cur_line = false,
 })
